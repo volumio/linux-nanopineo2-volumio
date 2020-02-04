a178d2c9d1ed2bb48ea40576c3acbeb217ae186d
gkkpch <ge.koerkamp@gmail.com>
2020-02-04 10:58:08 +0100

    Forked from https://github.com/nikkov/friendlyarm-linux , added dtb's

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 6 */
+/* SMP */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#6 SMP Tue Feb 4 10:55:50 CET 2020"
+#define LINUX_COMPILE_BY "nanopi"
+#define LINUX_COMPILE_HOST "busteros"
+#define LINUX_COMPILER "gcc version 6.3.1 20170109 (Linaro GCC 6.3-2017.02) "
